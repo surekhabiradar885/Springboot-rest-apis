434f12869b20170677b0e0fa8d1e8dac302fc992
Surekha Biradar <surekha.biradar@mtn.com>
2022-11-22 19:43:26 +0530

    add : setting up project

---
+++ src/test/java/com/springboot/restspringboot/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.restspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
