02db69a5106eeafa46eff9e371f5905d75fd64c1
Surekha Biradar <surekha.biradar@mtn.com>
2022-11-22 19:51:58 +0530

    add : courses api

---
+++ src/main/java/com/springboot/restspringboot/CourseController.java
@@ -0,0 +1,19 @@
+package com.springboot.restspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	@RequestMapping("/courses")
+	public List<Course> retriveAllCourses(){
+		return Arrays.asList(
+				new Course(1,"Learn AWS","Amazon"),
+				new Course(2,"Learn java","JavaPoint"));
+	}
+
+}
